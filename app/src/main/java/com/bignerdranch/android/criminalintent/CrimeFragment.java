6511b00b07764db99f995b76bb0d537f2b92b724
lml0608 <liumeile0608@163.com>
2017-02-08 19:32:35 +0800

    输出日期2017年2月8日

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -61,7 +62,10 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
+
+        //mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setText(DateFormat.getMediumDateFormat(getActivity()).format(mCrime.getDate()));
+
         mDateButton.setEnabled(false);
 
 
